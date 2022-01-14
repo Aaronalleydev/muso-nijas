57cfd15d85195d1377429e07e01e3c65cc6eebe9
Aaron Alley <aaronalleydev@gmail.com>
2022-01-14 13:05:33 +0000

    adding firebase storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
 
@@ -23,8 +24,9 @@ firebase.initializeApp(firebaseConfig)
 // Init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // Timestampo
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp}
